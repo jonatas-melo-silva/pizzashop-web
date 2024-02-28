015116ef3130f24c5c6b68371eee6559c7a53732
Jonatas Melo Silva <jonatas891024@gmail.com>
2024-02-28 00:17:17 -0300

    feat: adiciona layout e pagina de login

--- src/layouts/auth-layout.tsx
+++ src/layouts/auth-layout.tsx
@@ -1,17 +1,28 @@
 import { cva } from 'class-variance-authority'
+import { Pizza } from 'lucide-react'
 import { ComponentProps } from 'react'
 import { Outlet } from 'react-router-dom'
 
 import { cn } from '@/libs'
 
-const styles = cva('')
+const styles = cva('min-h-screen grid grid-cols-2')
 
 export type AuthLayoutProps = ComponentProps<'div'>
 
 export function AuthLayout({ className, ...props }: AuthLayoutProps) {
   return (
     <div className={cn(styles({ className }))} {...props}>
-      <main>
+      <aside className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <section className="flex items-center gap-3 text-lg font-medium text-foreground">
+          <Pizza className="h-5 w-5" />
+          <span className="font-semibold">pizza.shop</span>
+        </section>
+        <footer className="text-sm">
+          Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
+        </footer>
+      </aside>
+
+      <main className="flex flex-col items-center justify-center">
         <Outlet />
       </main>
     </div>
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -2,9 +2,10 @@ import { cva } from 'class-variance-authority'
 import { ComponentProps } from 'react'
 import { Helmet } from 'react-helmet-async'
 
+import { Button, Input, Label } from '@/components/ui'
 import { cn } from '@/libs'
 
-const styles = cva('')
+const styles = cva('p-8')
 
 export type SignInProps = ComponentProps<'div'>
 
@@ -13,7 +14,25 @@ export function SignIn({ className, ...props }: SignInProps) {
     <>
       <Helmet title="Login" />
       <div className={cn(styles({ className }))} {...props}>
-        SignIn
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <section className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </section>
+
+          <form className="flex flex-col gap-4">
+            <fieldset>
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" />
+            </fieldset>
+
+            <Button type="submit">Acessar painel</Button>
+          </form>
+        </div>
       </div>
     </>
   )
