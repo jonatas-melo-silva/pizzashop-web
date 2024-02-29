1f5c4a47acca43f6ece46c44a66e6e3895b27ca1
Jonatas Melo Silva <jonatas891024@gmail.com>
2024-02-29 17:53:42 -0300

    feat: adiciona funcionalidade de logout automatico

--- src/layouts/app-layout.tsx
+++ src/layouts/app-layout.tsx
@@ -1,15 +1,38 @@
+import { isAxiosError } from 'axios'
 import { cva } from 'class-variance-authority'
-import { ComponentProps } from 'react'
-import { Outlet } from 'react-router-dom'
+import { ComponentProps, useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 import { Header } from '@/components'
-import { cn } from '@/libs'
+import { api, cn } from '@/libs'
 
 const styles = cva('flex nim max-h-screen flex-col antialiased')
 
 export type AppLayoutProps = ComponentProps<'div'>
 
 export function AppLayout({ className, ...props }: AppLayoutProps) {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (isAxiosError(error)) {
+          const status = error.response?.status
+          const code = error.response?.data.code
+
+          if (status === 401 && code === 'UNAUTHORIZED') {
+            navigate('/sign-in', { replace: true })
+          }
+        }
+      },
+    )
+
+    return () => {
+      api.interceptors.response.eject(interceptorId)
+    }
+  }, [navigate])
+
   return (
     <div className={cn(styles({ className }))} {...props}>
       <Header />
