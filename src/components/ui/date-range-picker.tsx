b8186abe627283b02ada229cf2c9e40a4a01d623
Jonatas Melo Silva <jonatas891024@gmail.com>
2024-02-29 22:36:38 -0300

    feat: adiciona metricas da receita do periodo

---
+++ src/components/ui/date-range-picker.tsx
@@ -0,0 +1,60 @@
+import { format } from 'date-fns'
+import { Calendar as CalendarIcon } from 'lucide-react'
+import * as React from 'react'
+import { DateRange } from 'react-day-picker'
+
+import { cn } from '@/libs'
+
+import { Button, Calendar, Popover, PopoverContent, PopoverTrigger } from '.'
+
+interface DateRangePickerProps extends React.ComponentProps<'div'> {
+  readonly date: DateRange | undefined
+  readonly onDateChange: (date: DateRange | undefined) => void
+}
+
+export function DateRangePicker({
+  date,
+  onDateChange,
+  className,
+}: DateRangePickerProps) {
+  return (
+    <div className={cn('grid gap-2', className)}>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            id="date"
+            variant={'outline'}
+            className={cn(
+              'w-[300px] justify-start text-left font-normal',
+              !date && 'text-muted-foreground',
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date?.from ? (
+              date.to ? (
+                <>
+                  {format(date.from, 'LLL dd, y')} -{' '}
+                  {format(date.to, 'LLL dd, y')}
+                </>
+              ) : (
+                format(date.from, 'LLL dd, y')
+              )
+            ) : (
+              <span>Pick a date</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            initialFocus
+            mode="range"
+            defaultMonth={date?.from}
+            selected={date}
+            onSelect={onDateChange}
+            numberOfMonths={2}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  )
+}
