c5d93f2238d1316b6c47af9e662c5d1a9194df88
Jonatas Melo Silva <jonatas891024@gmail.com>
2024-03-02 11:09:06 -0300

    test: adiciona mock pedidos e detalhes de pedido

--- src/api/mocks/handlers.ts
+++ src/api/mocks/handlers.ts
@@ -1,20 +1,18 @@
-import { setupWorker } from 'msw/browser'
-
-import { env } from '@/env'
-
 import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
 import { getDayOrdersAmountMock } from './get-day-orders-amount-mock'
 import { getManagedRestaurantMock } from './get-managed-restaurant-mock'
 import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
 import { getMonthOrdersAmountMock } from './get-month-orders-amount-mock'
 import { getMonthRevenueMock } from './get-month-revenue-mock'
+import { getOrderDetailsMock } from './get-order-details-mock'
+import { getOrdersMock } from './get-orders-mock'
 import { getPopularProductsMock } from './get-popular-products-mock'
 import { getProfileMock } from './get-profile-mock'
 import { registerRestaurantMock } from './register-restaurant-mock'
 import { signInMock } from './sign-in-mock'
 import { updateProfileMock } from './update-profile-mock'
 
-export const worker = setupWorker(
+export const handlers = [
   signInMock,
   registerRestaurantMock,
   getDayOrdersAmountMock,
@@ -26,10 +24,6 @@ export const worker = setupWorker(
   getProfileMock,
   getManagedRestaurantMock,
   updateProfileMock,
-)
-
-export async function enableMSW() {
-  if (env.MODE !== 'test') return
-
-  await worker.start()
-}
+  getOrderDetailsMock,
+  getOrdersMock,
+]
